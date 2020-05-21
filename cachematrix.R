d9038931dd0001433d76418f56f045ce81a34bb9
Qiyang Zhang <zhangqiyang0329@gmail.com>
2020-05-21 13:56:01 -0400

    Finished assignment

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,30 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  q <- NULL
+  set <- function(y) {
+    x <<- y
+    q <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) q <<- inverse
+  getinverse <- function() q
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  q <- x$getinverse()
+  if(!is.null(q)) {
+    message("getting cached data")
+    return(q)
+  }
+  data <- x$get()
+  q <- solve(data, ...)
+  x$setinverse(q)
+  q ## Return a matrix that is the inverse of 'x'
 }
